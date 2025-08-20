7bfa6cfe81ecdcf534be4dd066ae1d47d49f6378
Vipul Kale <120079040+Vipulkale45@users.noreply.github.com>
2025-08-20 11:19:54 +0530

    Merge two Array in another array

---
+++ Merge2Array.java
@@ -0,0 +1,20 @@
+package JAP82.Array;
+
+import java.util.Arrays;
+
+public class Merge2Array {
+    public static void main(String[] args) {
+        int a[]={10,20,30,40,50};
+        int b[]={11,22,33,44,55};
+        int c[]=new int[a.length+b.length];
+        for(int i=0; i<a.length; i++)
+        {
+            c[i]=a[i];
+        }
+        for(int i=0; i<b.length; i++)
+        {
+            c[a.length+i]=b[i];
+        }
+        System.out.println(Arrays.toString(c));
+    }
+}
