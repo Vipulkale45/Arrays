e85f4070060b52869244290dfb0d4145cc326474
Vipul Kale <120079040+Vipulkale45@users.noreply.github.com>
2025-08-20 10:51:07 +0530

    Easy to solve the prefixSumOfArray

---
+++ prefixsumarray.java
@@ -0,0 +1,18 @@
+package JAP82.Array;
+
+import java.util.Arrays;
+
+public class prefixsumarray {
+    public static void main(String[] args) {
+        int a[]={1,2,3,4,5};
+        int b[]=new int[a.length];
+        b[0]=a[0];
+        for(int i=1; i<a.length; i++)
+        {
+            b[i]=b[i-1]+a[i];
+        }
+        System.out.println(Arrays.toString(b));
+    }
+}
+//input : {1,2,3,4,5}
+//output: [1, 3, 6, 10, 15]
